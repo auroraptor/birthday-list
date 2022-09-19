332e4ce7e4f0a2e9c8cba25c548d09f090c63860
Александр Бондарай <bondoray@live.com>
2022-09-19 21:00:13 +0200

    feat: добавляет новые компоненты в контейнер

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+import BdForm from '../BdForm/BdForm';
 import styles from './App.module.css';
 
 function App() {
+  const [bdList, setBdList] = useState([
+    {
+      id: 1,
+      date: "21.06"
+    },
+    {
+      id: 2,
+      date: "01.01"
+    }
+  ]);
+
+
   return (
     <div className={styles.App}>
       <div className={styles.Content}>
-        
+        <BdForm setBdList={setBdList} />
+
       </div>
     </div>
   );
