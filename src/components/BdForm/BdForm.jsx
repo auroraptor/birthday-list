d81a9005226fc0a8929abf115762086b211dd6f0
Александр Бондарай <bondoray@live.com>
2022-09-25 08:22:01 +0200

    feat: подключает redux

--- src/components/BdForm/BdForm.jsx
+++ src/components/BdForm/BdForm.jsx
@@ -1,17 +1,20 @@
 import { React, useState } from "react";
+import { useDispatch } from "react-redux/es/exports";
+import { addBd } from "../../slices/bdSlice";
 import BdButton from "../UI/BdButton/BdButton";
 import BdInput from "../UI/BdInput/BdInput";
 import styles from "./bd-form.module.css";
 
-export default function BdForm({ setBdList }) {
+export default function BdForm() {
   const [bd, setBd] = useState("");
-
+  const dispatch = useDispatch();
   const inputHandle = (evt) => {
     setBd(evt.target.value);
   };
   const addHandle = (evt) => {
     evt.preventDefault();
-    setBdList((prevList) => [...prevList, { id: new Date(), date: bd }]);
+    //TODO:генерация id от сервера
+    dispatch(addBd({ id: new Date(), date: bd }));
   };
 
   return (
